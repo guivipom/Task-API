9dc434082010c9e448410f6c8c1651e68b083e8b
guivipom <guivipom@gmail.com>
2020-09-14 19:09:39 +0200

    Refactor promise chaining for async\await

--- src/index.js
+++ src/index.js
@@ -2,77 +2,136 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { translateAliases } = require('./models/user')
 
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json())
 
-app.post('/users', (req,res ) => {
+app.post('/users',  async (req,res ) => {
     const user = new User(req.body)
 
-    user.save().then(()=>{
+    try{
+        await user.save()
         res.status(201).send(user)
-
-
-    }).catch((e)=> {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/users', (req,res)=>{
-    User.find({}).then((users) =>{
+app.get('/users', async (req,res)=>{
+
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
+
 })
 
-app.get('/users/:id', (req,res)=> {
+app.get('/users/:id', async (req,res)=> {
     const _id = req.params.id
 
-    User.findById(_id).then((user)=> {
+    try {
+        const user = await User.findById(_id)
+
         if(!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e)=> {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
 } )
 
-app.post('/tasks', (req,res ) => {
+app.post('/tasks', async (req,res ) => {
     const task = new Task(req.body)
 
-    task.save().then(()=>{
+    try {
+        await task.save()
         res.status(201).send(task)
-
-
-    }).catch((e)=> {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/tasks', (req,res)=>{
-    Task.find({}).then((task) =>{
-        res.send(task)
-    }).catch((e) => {
+app.get('/tasks', async (req,res)=>{
+
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
         res.status(500).send(e)
-    })
+        
+    }
 })
 
-app.get('/tasks/:id', (req,res)=> {
+app.get('/tasks/:id', async (req,res)=> {
     const _id = req.params.id
 
-    Task.findById(_id).then((task)=> {
-        if(!task){
+    try {
+
+        const task = await Task.findById(_id)
+        if(!task) {
             return res.status(404).send()
         }
-        res.send(task)
-    }).catch((e)=> {
+
+        res.send(task)      
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 } )
+
+app.patch('/users/:id', async (req,res)=> {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
+    
+    if(!isValidOperation){
+        return res.status(400).send({ error : 'Invalid property!'})
+    }
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body , 
+                                    { new: true, runValidators: true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+} )
+
+app.patch('/tasks/:id', async (req,res)=> {
+    const _id = req.params.id
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
+    console.log(isValidOperation)
+    
+    if(!isValidOperation){
+        console.log('invalid operation')
+        return res.status(400).send({ error : 'Invalid property!'})
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(_id , req.body,
+                                            { new: true, runValidators: true})
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)      
+    } catch (error) {
+        res.status(500).send()
+    }
+} )
+
 
 
 
